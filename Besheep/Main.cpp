6293dbe1b5b6b8eb2a59dff7277f47b306549350
bault18 <bault18@my.whitworth.edu>
2016-11-01 13:38:45 -0700

    Final working algorithm. -Fixed issue with srt function where it would incorrectly return true -added 2 input edge case now that points are sorted -Corrected the remove duplicates for after the quickhull algorithm runs

--- Besheep/Main.cpp
+++ Besheep/Main.cpp
@@ -23,22 +23,16 @@ public:
 	}
 };
 
-/*
-bool operator == (point p1, point p2)
-{
-	return (p1.y == p2.y) && (p1.x == p2.x);
-}*/
-
 //used to sort points
 //chooses lowest y, then lowest x, then lowest input point
 bool srt(point p1, point p2)
 {
-	//see which has greater y coord
+	//see which has lesser y coord
 	if (p1.y < p2.y)
 		return true;
 	if (p1.y > p2.y)
-		return true;
-	//see which has greater x coord
+		return false;
+	//see which has lesser x coord
 	if (p1.x < p2.x)
 		return true;
 	if (p1.x > p2.x)
@@ -109,7 +103,7 @@ vector<point> lower_hull(point p1, point p2, vector<point> points)
 	vector<point> newpts;
 	for (int i = 0; i < points.size(); i++)
 	{
-		point p3 = points.at(i);
+		point p3 = points[i];
 
 		double currh = ((p1.x * (p2.y - p3.y)) + (p2.x * (p3.y - p1.y)) + (p3.x * (p1.y - p2.y)));
 
@@ -169,13 +163,13 @@ int main()
 		}
 
 
-		//edge cases
+		//Edge Cases
 		if (num_input == 0)
 		{
 			cout << "0.00" << endl << endl;
 			continue;
 		}
-		else if (num_input == 1)
+		if (num_input == 1)
 		{
 			cout << "0.00" << endl;
 			cout << "1" << endl << endl;
@@ -191,11 +185,21 @@ int main()
 
 		points.resize(distance(points.begin(), it));
 		
+		//One more edge
+		if (num_input == 2)
+		{
+			double circumference = 2 * sqrt(abs(pow((points[0].x - points[1].x), 2) + pow((points[0].y - points[1].y), 2)));
+			cout << fixed << setprecision(2) << circumference << endl;
+			cout << points[0].num << " " << points[1].num << endl << endl;
+			continue;
+		}
 
 		//~~~~~~~~~~~Finding Edge Points~~~~~~~~~~~
 
 		point left_most = points.front();
 		point right_most = points.back();
+		points.erase(points.begin());
+		points.erase(points.begin() + points.size() - 1);
 
 		//~~~~~~~~~~Run Quickhull~~~~~~~~~~
 
@@ -204,13 +208,14 @@ int main()
 		vector<point> lowerhull = lower_hull(left_most, right_most, points);
 
 		vector<point> completehull = lowerhull;
+
+
 		for (int i = 0; i < upperhull.size(); i++)
 		{
 			completehull.push_back(upperhull.at(i));
 		}
 
-		if (completehull.size() == 2)
-			completehull.push_back(left_most);
+		completehull.push_back(left_most);
 
 		/* Remove Duplicates
 		* -This is important if it is in a line
@@ -218,8 +223,11 @@ int main()
 		for (int i = 0; i < completehull.size() - 2; i++)
 		{
 			for (int j = i + 1; j < completehull.size() - 1; j++)
-				if (unique_pts(completehull[i],completehull[j]))
+				if (unique_pts(completehull[i], completehull[j]))
+				{
 					completehull.erase(completehull.begin() + j);
+					j--;
+				}
 		}
 
 		//Calculate circumference
